24ede659dac02994e8ac220b52f1704726de2579
Amaan <hawaldaramaan@gmail.com>
2026-07-06 21:36:47 +0530

    Contains_Duplicate_II

---
+++ src/main/java/com/algorithmsPractice/HashMap/Contains_Duplicate_II.java
@@ -0,0 +1,19 @@
+package com.algorithmsPractice.HashMap;
+
+import java.util.HashMap;
+
+public class Contains_Duplicate_II {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int j = map.get(nums[i]);
+                if(Math.abs(i-j)<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}

e53c915ff716a79f19454f3396e01b0601abd1d7
Amaan <hawaldaramaan@gmail.com>
2026-07-07 00:15:24 +0530

    Find_the_Difference

---
+++ src/main/java/com/algorithmsPractice/HashMap/Find_the_Difference.java
@@ -0,0 +1,27 @@
+package com.algorithmsPractice.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Find_the_Difference {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+        for (int j = 0; j < t.length(); j++) {
+            char ch2 = t.charAt(j);
+            map2.put(ch2, map2.getOrDefault(ch2, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map2.entrySet()) {
+            if(entry.getValue()!=map1.getOrDefault(entry.getKey(), 0)){
+                return entry.getKey();
+            }
+        }
+        return '\0';
+    }
+}

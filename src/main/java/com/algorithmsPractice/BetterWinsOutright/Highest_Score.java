b8a703b094617f366e90d4ac6a8573a44a1c0b58
Amaan <hawaldaramaan@gmail.com>
2026-07-08 21:03:26 +0530

    Highest_Frequencies

---
+++ src/main/java/com/algorithmsPractice/BetterWinsOutright/Highest_Score.java
@@ -0,0 +1,8 @@
+package com.algorithmsPractice.BetterWinsOutright;
+
+public class Highest_Score {
+    //You're given an array of scores for players standing in a line (index 0 = first in line).
+    // Return the index of the player with the highest score.
+    // If multiple players tie for the highest score, return the index of the one standing earliest (smallest index).
+
+}

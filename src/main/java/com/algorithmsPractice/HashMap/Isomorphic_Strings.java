7ea5da7b5c0ab483b9183fab7116327a015599ab
Amaan <hawaldaramaan@gmail.com>
2026-07-10 23:18:35 +0530

    HappyNumber

---
+++ src/main/java/com/algorithmsPractice/HashMap/Isomorphic_Strings.java
@@ -0,0 +1,28 @@
+package com.algorithmsPractice.HashMap;
+
+import java.util.HashMap;
+
+public class Isomorphic_Strings {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if ((map.get(s.charAt(i))!=t.charAt(i))) {
+                    return false;
+                }
+            }
+            else {
+                if(!map.containsValue(t.charAt(i))){
+                    map.put(s.charAt(i), t.charAt(i));
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
